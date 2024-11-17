6c7e3d21ca8d4afcb052eb7ed3c3a29317e7c689
Fahad Ghaffar <fahadghaffar.awan@gmail.com>
2024-11-17 07:17:10 +0500

    Fix migration files

--- migrations/20241116213408-create-user.js
+++ migrations/20241116213408-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("users", {
+    await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -18,6 +18,10 @@ module.exports = {
       phone: {
         type: Sequelize.INTEGER,
       },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
       user_type: {
         type: Sequelize.ENUM("user", "admin"),
       },
@@ -32,6 +36,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("users");
+    await queryInterface.dropTable("Users");
   },
 };
--- migrations/20241116213724-create-quotation.js
+++ migrations/20241116213724-create-quotation.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("quotations", {
+    await queryInterface.createTable("Quotations", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -12,7 +12,7 @@ module.exports = {
       user_id: {
         type: Sequelize.INTEGER,
         references: {
-          model: "users",
+          model: "Users",
           key: "id",
         },
         allowNull: false,
@@ -43,6 +43,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("quotations");
+    await queryInterface.dropTable("Quotations");
   },
 };
