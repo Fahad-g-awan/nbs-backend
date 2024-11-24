77ceeafa75d2f81f0e5ff3586092ddc0c9e5adfd
Fahad Ghaffar <fahadghaffar.awan@gmail.com>
2024-11-24 18:25:35 +0500

    Fix cors error

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -53,5 +53,3 @@ sequelize
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("Database connection failed:", err));
-
-
